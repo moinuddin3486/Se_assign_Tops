864a2759d1579a3213e458df67fabaa30c7e176b
Momin Moinuddin <151716875+moinuddin3486@users.noreply.github.com>
2024-06-05 12:47:01 +0530

    Assignment

--- module 4 python/What is oops.py
+++ module 4 python/What is oops.py
@@ -6,12 +6,10 @@ Ans :
 
             Encapsulation: This is the bundling of data and methods that operate on the data into a single unit or class. It hides the internal state of an object from the outside world and only exposes the necessary functionalities.
 
-            Inheritance: Inheritance is a mechanism by which a new class can inherit properties and behavior (methods) from an existing class. It promotes code reusability and allows the creation of a hierarchy of classes.
+            Inheritance: create new class by exist
 
-            Polymorphism: Polymorphism means the ability to take on different forms. In OOP, it refers to the ability of different objects to respond to the same message (method call) in different ways. Polymorphism can be achieved through method overriding and method overloading.
+            Polymorphism: One Name Multiple Forms
 
-            Abstraction: Abstraction refers to the concept of hiding the complex implementation details and showing only the essential features of the object. It allows the programmer to focus on what an object does rather than how it does it.
+            Abstraction:For Data Hiding
 
-            multiple inheritance in Java:
-
-            No, Java does not support multiple inheritance of classes
+            No Java does not support multiple inheritance of classes
